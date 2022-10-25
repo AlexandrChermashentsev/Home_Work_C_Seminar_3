a442e3966774da882bec88d1c23c757f287d51ff
Alexandr.Cherm <Alexandr.chermashentsev@gmail.com>
2022-10-25 14:28:09 +0500

    Add Ex23

---
+++ Ex_23/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 23
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+Console.WriteLine("Enter the number");
+int number = int.Parse(Console.ReadLine());
+int[] cube = new int[number];
+int digit = 1;
+for (int i = 0; i < number; i++)
+{
+    cube[i] = digit * digit * digit;
+    digit++;
+}
+Console.Write($"{number} -> ");
+Console.WriteLine(string.Join(", ", cube));
